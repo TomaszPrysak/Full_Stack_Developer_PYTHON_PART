790587d694c784e88c30998f1037817418d81613
Tomasz Prysak <pitek@macbook-pro-tomasz.home>
2019-01-22 23:41:26 +0100

    Przegladam podstawy Pythona, moze bedzie cos czego nie wiem

---
+++ PYTHON_PART_ONE/Part2_Strings_my.py
@@ -0,0 +1,6 @@
+my_string1 = "testujemy"
+# sposob iterowania od konca
+print(my_string1[::-1])
+# sposob umieszczania stringow w stringu
+my_string2 = "Tutaj wstawimy {x}, a tutaj {y}".format(x="nic",y="cos")
+print(my_string2)
