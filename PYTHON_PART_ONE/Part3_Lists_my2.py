70944001046627ff74918e1901165c0b156df828
TomaszPrysak <prysak.tomasz@gmail.com>
2019-01-24 23:18:21 +0100

    Dalsze przeglądanie python i robienie notatek

--- PYTHON_PART_ONE/Part3_Lists_my2.py
+++ PYTHON_PART_ONE/Part3_Lists_my2.py
@@ -6,6 +6,6 @@
 
 lista = [[1,2,3],[4,5,6],[7,8,9]]
 firstColumn = [item[0] for item in lista] # jednolinijkowe wyrazenie, które iteruje przez wszystkie elementy listy
-# i dokonuje na elementach przez ktore iteruje jakichś opracji. W tym przypadku z pobiera pierwszy element list
-# będących elementami zbiorczej listy
+# i zwraca elementy według określonego przez nas warunku. Warunek ten stoi na samym początku wyrażenia.
+# W tym przypadku z pobiera pierwszy element list będących elementami zbiorczej listy i umieszcza w nowej liście
 print(firstColumn)
---
+++ PYTHON_PART_ONE/Part4_Dictionaries_my.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Słowniki są to zbiory pary klucz/wartość. Jednak pierwotne zdefiniowanie słownika z
+# parami klucz/wartość nie określa jednocześnie takiej kolejności. Wszystko co umieszczamy w słowniku
+# nie ma określonego położenia w nim. Jeżeli byśmy iterowali przez słownik to pary klucz/wartość
+# będą w losowej kolejności.
+
+# Słowniki w Pythonie posiadają wbudowane metody do inkrementowania, dekrementowania, mnożenia oraz dzielenia
+# wartości przypisanej do klucza. Odbuwa się to tak samo jako te same operacje jakiekolwiek zmiennej.
+# Wykorzystujemy:
+# +=
+# -=
+# *=
+# /=
+
+dict = {"key1":3, "key2":5}
+print(dict["key1"])
+dict["key1"] += 1
+print(dict["key1"])
+dict["key1"] *= 2
+print(dict["key1"])
+dict["key2"] -= 1
+print(dict["key2"])
+dict["key2"] /= 2
+print(dict["key2"])
+
+print(dict.keys()) # zwraca listę kluczy w słowniku, list ta będzie nieposortowana
+print(dict.values()) # listę wartości przypisanych do kluczy, lista ta również nie będzie posortowana
+print(dict.items()) # zwraca listę krotek, ilość par klucz/wartość oznacza taką samą ilość zwróconych krotek.
+# każda krotka będzie złożona z klucza i przypisanej do niej wartości.
